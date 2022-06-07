aab0e15811a42587488f11d52f8f16a1c735d00e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-07 14:49:16 +0300

    feat: add 5 lesson

---
+++ 3. Basic types/05 Arrays/app.ts
@@ -0,0 +1,10 @@
+const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+
+for (const skill of skills) {
+  console.log(skill.toLowerCase());
+}
+
+const res = skills.filter((s: string) => s !== 'Devops').map(s => s + '! ')
+.reduce((a, b) => a + b)
+
+console.log(res);
