9ad130a44c184834e160762b619acaab2477329f
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-08 23:39:33 +0300

    feat: add 3 lesson

---
+++ 04 Advanced types/03 Type Aliases/app.js
@@ -0,0 +1,17 @@
+"use strict";
+function fetchWithAuth(url, method) {
+    return 1;
+}
+fetchWithAuth('s', 'post');
+let method = 'post';
+fetchWithAuth('s', method);
+// type UserWithRole = {
+//   user: User,
+//   role: Role
+// }
+let user = {
+    name: 'John',
+    age: 30,
+    skills: ['1', '2'],
+    id: 1
+};
