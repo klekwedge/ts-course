4cadcc96cfdf1cb38f977e63b8f7aaf6b35e7323
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-09 15:29:51 +0300

    feat: add 7 lesson

---
+++ 4. Advanced types/07 Exercise - Typing the server response/app.ts
@@ -0,0 +1,36 @@
+interface IPayment {
+  sum: number,
+  from: number,
+  to: number
+}
+
+enum PaymentStatus {
+  Success = 'success',
+  Failed = 'failed'
+}
+
+interface IPaymentRequest extends IPayment { }
+
+interface IDataSuccess extends IPayment{
+  databaseId: number,
+}
+
+interface IDataFailed {
+  errorMessage: string,
+  errorCode: number,
+}
+
+// interface IResponse {
+//   status: PaymentStatus,
+//   data: IDataSuccess | IDataFailed
+// }
+
+interface IResponseSuccess {
+  status: PaymentStatus.Success,
+  data: IDataSuccess
+}
+
+interface IResponseFailed {
+  status: PaymentStatus.Failed,
+  data: IDataFailed
+}
