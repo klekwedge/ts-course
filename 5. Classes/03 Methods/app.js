cbd48b21f39f92c722e2c1a24ad770d69de87ab0
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-11 18:35:09 +0300

    feat: add 3 lesson

---
+++ 5. Classes/03 Methods/app.js
@@ -0,0 +1,31 @@
+"use strict";
+var PaymentStatus;
+(function (PaymentStatus) {
+    PaymentStatus[PaymentStatus["Holded"] = 0] = "Holded";
+    PaymentStatus[PaymentStatus["Processed"] = 1] = "Processed";
+    PaymentStatus[PaymentStatus["Reversed"] = 2] = "Reversed";
+})(PaymentStatus || (PaymentStatus = {}));
+class Payment {
+    constructor(id) {
+        this.status = PaymentStatus.Holded;
+        this.createdAt = new Date();
+        this.id = id;
+    }
+    getPaymentLifeTime() {
+        return new Date().getTime() - this.createdAt.getTime();
+    }
+    unholdPayment() {
+        if (this.status === PaymentStatus.Processed) {
+            throw new Error('Платеж не может быть возвращен!');
+        }
+        else {
+            this.status = PaymentStatus.Reversed;
+            this.updatedAt = new Date();
+        }
+    }
+}
+const payment = new Payment(1);
+payment.unholdPayment();
+console.log(payment);
+const time = payment.getPaymentLifeTime();
+console.log(time);
---
+++ 5. Classes/03 Methods/app.ts
@@ -0,0 +1,37 @@
+enum PaymentStatus {
+  Holded,
+  Processed,
+  Reversed
+}
+
+
+class Payment {
+  id: number;
+  status: PaymentStatus = PaymentStatus.Holded;
+  createdAt: Date = new Date();
+  updatedAt: Date;
+
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  getPaymentLifeTime(): number {
+    return new Date().getTime() - this.createdAt.getTime();
+  }
+
+  unholdPayment(): void {
+    if (this.status === PaymentStatus.Processed) {
+      throw new Error('Платеж не может быть возвращен!')
+    }
+    else {
+      this.status = PaymentStatus.Reversed;
+      this.updatedAt = new Date();
+    }
+  }
+}
+
+const payment = new Payment(1);
+payment.unholdPayment();
+console.log(payment);
+const time = payment.getPaymentLifeTime();
+console.log(time);
