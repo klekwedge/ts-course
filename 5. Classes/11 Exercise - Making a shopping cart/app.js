647ea3da05bb18d5b4ca3f48f7b9ba94d4c00c1f
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-25 10:19:36 +0300

    feat: add 11 lesson

---
+++ 5. Classes/11 Exercise - Making a shopping cart/app.js
@@ -0,0 +1,61 @@
+"use strict";
+class Product {
+    constructor(id, title, price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
+class Delivery {
+    constructor(date) {
+        this.date = date;
+    }
+}
+class HomeDelivery extends Delivery {
+    constructor(date, address) {
+        super(date);
+        this.address = address;
+    }
+}
+class ShopDelivery extends Delivery {
+    constructor(shopId) {
+        super(new Date());
+        this.shopId = shopId;
+    }
+}
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+    addProduct(product) {
+        this.products.push(product);
+    }
+    deleteProduct(productId) {
+        this.products = this.products.filter((product) => product.id !== productId);
+    }
+    getSum() {
+        return this.products
+            .map((p) => p.price)
+            .reduce((p1, p2) => p1 + p2);
+    }
+    setDelivery(delivery) {
+        this.delivery = delivery;
+    }
+    checkout() {
+        if (this.products.length === 0) {
+            throw new Error('Нет ни одного товара в корзине');
+        }
+        if (!this.delivery) {
+            throw new Error('Не указан способ доставки');
+        }
+        return { success: true };
+    }
+}
+const cart = new Cart();
+cart.addProduct(new Product(1, 'Печенье', 10));
+cart.addProduct(new Product(2, 'Торт', 30));
+cart.addProduct(new Product(3, 'Шоколад', 20));
+cart.deleteProduct(1);
+cart.setDelivery(new HomeDelivery(new Date(), 'address'));
+console.log(cart.getSum());
+console.log(cart.checkout());
