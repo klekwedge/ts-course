31b37683d3987c0f95fc592808cec68059fc6808
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-25 11:53:26 +0300

    feat: add 15 lesson

---
+++ 5. Classes/15 Abstract classes/app.ts
@@ -0,0 +1,19 @@
+abstract class Controller {
+  abstract handle(req: any): void
+
+  handleWithLogs(req: any) {
+    console.log('Start');
+    this.handle(req)
+    console.log('End');
+  }
+}
+
+class UserController extends Controller {
+  handle(req: any): void {
+    console.log(req);
+  }
+}
+
+// new Controller() error
+const c = new UserController();
+c.handleWithLogs('Request')
